a91e2f7b92200a54cedfb3c30036f2fd033bef88
Vinay-m993 <71966836+Vinay-m993@users.noreply.github.com>
2021-01-13 20:51:53 +0530

    My Project_gitHub Repository

---
+++ Automation_Payroll/src/main/java/utilities/GenericUtility.java
@@ -0,0 +1,18 @@
+package utilities;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class GenericUtility {
+
+	// return current date
+	public static void currentDate(WebElement element, WebDriver driver, String locator) {
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+		LocalDateTime now = LocalDateTime.now();
+		System.out.println(dtf.format(now));
+	}
+
+}
